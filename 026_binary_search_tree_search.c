bae35c122a23cca1dcfce383dab1d06f2cb58320
cztqwan <895092337@qq.com>
2018-03-10 15:32:40 +0800

    添加二叉搜索树——搜索

---
+++ 026_binary_search_tree_search.c
@@ -0,0 +1,136 @@
+/*
+¶þ²æËÑË÷Ê÷¡ª¡ªËÑË÷
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Node
+{
+	int key;
+	struct Node *right, *left, *parent;
+}Node;
+
+Node *root, *NIL;
+
+void insert(int k)
+{
+	Node *y = NIL;
+	Node *x = root;
+	Node *z;
+	
+	z = (Node *)malloc(sizeof(Node));
+	z->key = k;
+	z->left = NIL;
+	z->right = NIL;
+	
+	while( x != NIL )
+	{
+		y = x;
+		if( z->key < x->key )
+		{
+			x = x->left;
+		}
+		else
+		{
+			x = x->right;
+		}
+	}
+	z->parent = y;
+	if( y == NIL )
+	{
+		root = z;
+	}
+	else{
+		if( z->key < y->key )
+		{
+			y->left = z;
+		}
+		else{
+			y->right = z;
+		}
+	}
+}
+
+Node * find(Node *u, int k)
+{
+	while( u != NIL && k != u->key )
+	{
+		if(k < u->key)
+			u = u->left;
+		else
+			u = u->right;
+	}
+	return u;
+}
+
+void inorder(Node *u)
+{
+	if(u == NIL)	return;
+	inorder(u->left);
+	printf(" %d", u->key);
+	inorder(u->right);
+}
+
+void preorder(Node *u)
+{
+	if(u == NIL)	return;
+	printf(" %d", u->key);
+	preorder(u->left);
+	preorder(u->right);
+}
+
+void postorder(Node *u)
+{
+	if(u == NIL)	return;
+	postorder(u->left);
+	postorder(u->right);
+	printf(" %d", u->key);
+}
+
+int main()
+{
+	int i, n, x;
+	char com[7];
+	scanf("%d",&n);
+	for(i = 0; i < n; i++)
+	{
+		scanf("%s", com);
+		if( strcmp(com,"insert")==0 )
+		{
+			scanf("%d", &x);
+			insert(x);
+		}
+		else if( strcmp(com,"print")==0 )
+		{
+			preorder(root);
+			printf("\n");
+			inorder(root);
+			printf("\n");
+			postorder(root);
+			printf("\n");
+		}
+		else if( strcmp(com,"find")==0 )
+		{
+			scanf("%d", &x);
+			Node *t = find(root, x);
+			if(t!=NIL)	printf("yes\n");
+			else	printf("no\n");
+		}
+	}
+	while(1);
+	return 0;
+}
+/*
+10
+insert 30
+insert 88
+insert 12
+insert 1
+insert 20
+find 12
+insert 17
+insert 25
+find 16
+print
+*/
